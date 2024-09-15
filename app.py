0959172bd2f1bf892efec573160e412b1c8a717c
Mattori Birnbaum <mattori.birnbaum@reaktor.com>
2024-09-15 16:57:43 +0900

    add history page

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask import Flask, g, render_template, request, url_for, send_from_directo
 import os
 from datetime import datetime
 import sqlite3
+from dataclasses import dataclass, astuple, asdict
+
+PAGINATION_SIZE = 10
 
 def create_app(test_config=None):
     Config.setup()
@@ -14,6 +17,7 @@ def create_app(test_config=None):
             db_con = sqlite3.connect(Config.DB)
             db_con.row_factory = sqlite3.Row
             db_cur = db_con.cursor()
+            db_cur.arraysize = PAGINATION_SIZE
             g.db_con = db_con
             g.db_cur = db_cur
 
@@ -38,6 +42,24 @@ def create_app(test_config=None):
         )
         db_con.commit()
 
+    @dataclass
+    class CaptureRow:
+        objects: list
+        run: int
+        model: str
+        timestamp: str
+
+    def process_row(row):
+        result = json.loads(row["result"])
+        objects = [{ "label": label, "score": float(score), "box": box, } for label, score, box in zip(result["labels"], result["scores"], result["boxes"])]
+        objects.sort(key=lambda item: item["score"], reverse=True)
+        return CaptureRow(
+            objects=objects,
+            run=row["created_at"],
+            model=row["model"],
+            timestamp=datetime.fromtimestamp(row["created_at"]).isoformat(sep=" ", timespec="seconds")
+        )
+
     @app.cli.command("capture")
     def capture():
         pass
@@ -48,20 +70,11 @@ def create_app(test_config=None):
         row = db_cur.execute("SELECT model, result, filename, created_at FROM captures LIMIT 1").fetchone()
         if row is None:
             return render_template("empty_feed.html")
-        result = json.loads(row["result"])
-        objects = [{ "label": label, "score": float(score), "box": box, } for label, score, box in zip(result["labels"], result["scores"], result["boxes"])]
-        objects.sort(key=lambda item: item["score"], reverse=True)
-        return render_template(
-            "feed.html",
-            run=row["created_at"],
-            model=row["model"],
-            objects=objects,
-            timestamp=datetime.fromtimestamp(row["created_at"]).isoformat(sep=" ", timespec="seconds")
-        )
+        objects = process_row(row)
+        return render_template("feed.html", **asdict(objects))
 
     @app.route("/image/<run>")
     def image(run):
-        print('run', run)
         (db_con, db_cur) = get_db()
         row = db_cur.execute("SELECT filename FROM captures WHERE created_at = ?", (run,)).fetchone()
         if "annotated" in request.args:
@@ -69,5 +82,14 @@ def create_app(test_config=None):
         else:
             dir = Config.ORIG_DIR
         return send_from_directory(dir, row["filename"])
+
+    @app.route("/history")
+    def history():
+        (db_con, db_cur) = get_db()
+        rows = db_cur.execute("SELECT model, result, filename, created_at FROM captures LIMIT ?", (PAGINATION_SIZE,)).fetchmany()
+        if len(rows) == 0:
+            return render_template("empty_feed.html")
+        captures = map(process_row, rows)
+        return render_template("history.html", captures=map(lambda cap: astuple(cap), captures))
 
     return app
